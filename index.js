6f092b0690bc01c9a9283cc7426fa02e78e74a74
Rafael <rafaelariovaldo02@gmail.com>
2022-04-06 14:40:04 -0300

    update code

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-app.get('/', (reqest, response) => {
+app.get('/users/:id', (request, response) => {
+    const params = request.query;
+    console.log(params);
+
     return response.json({
         evento: 'semana Oministeck 11.0',
         aluno: 'Rafael',
     });
 
-})
+});
 
 
 app.listen(3333);
